ffcc5216b4779431a99c30f6c0b737f0e2524e9b
Admin <admin@soidudu-2.local>
2024-12-14 21:36:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ const App = () => {
   return (
       <div className="container text-center my-5">
         <div class="row justify-content-center">
-        <div className='col-8'>
           <header className="text-center mb-4">
               <h1 className="display-4">Number Sums</h1>
               <p className="lead">Challenge your math skills with this exciting puzzle game!</p>
           </header>
           
           <GameBoard />
-          </div>
+          
           </div>
       </div>
   );
